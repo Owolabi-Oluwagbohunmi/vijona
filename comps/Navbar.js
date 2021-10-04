261490ea1a6ca8e37a4ab09a462e47b4203602e7
unknown <samuelowolabi.89@gmail.com>
2021-10-04 13:23:30 +0100

    Navbar links

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,23 +1,27 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { render } from 'react-dom'
 
 
 const Navbar = () => {
-    <nav className='flex w-100 justify-between'>
-        <a>Vijona</a>
+    return (
+        <nav className='flex w-100 justify-between'>
+            <a>Vijona</a>
 
-        <div>
-            <a>Home</a>
-            <a>Home</a>
-            <a>Home</a>
-            <a>Home</a>
-        </div>
+            <div>
+                <a>Home</a>
+                <a>What we do</a>
+                <a>Events&amp;Stories</a>
+                <a>Alumni Community</a>
+            </div>
 
-        <div>
-            <a>Donate</a>
-            <a>Join Us</a>
-        </div>
-    </nav>
+            <div>
+                <a>Donate</a>
+                <a>Join Us</a>
+            </div>
+        </nav>
+    )
+
 }
  
 export default Navbar;
