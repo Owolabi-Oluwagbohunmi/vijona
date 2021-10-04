321b36a4e3157422e272914bbcbf01594f9c67b0
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-10-04 17:13:21 +0100

    Add Navbar code for desktop screens

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,22 +1,39 @@
 import Image from "next/image";
 import Link from "next/link";
+import Logo from '../public/assets/vijona-black.jpg';
 
 const Navbar = () => {
   return (
-    <nav className="bg-black flex w-100 justify-between">
-      <a>Vijona</a>
+    <nav className="flex justify-between py-8 items-center">
+      <div className=" w-28 md:w-36 cursor-pointer border p-0 ">
+            <Image className="w-full  " src={Logo} alt="vijona-logo" />
+          </div>
 
-      <div>
-        <a>Home</a>
-        <a>Home</a>
-        <a>Home</a>
-        <a>Home</a>
+      <div className="hidden lg:flex">
+        <a className="cursor-pointer mx-8 ">About Us</a>
+        <a className="cursor-pointer mx-8 ">What we Do</a>
+        <a className="cursor-pointer mx-8 ">Events & Stories</a>
+        <a className="cursor-pointer mx-8">Alumni Community</a>
       </div>
 
-      <div>
-        <a>Donate</a>
-        <a>Join Us</a>
+      <div className="hidden lg:flex">
+        <a className="bg-v-yellow mr-14 px-6 py-3 rounded-sm text-white">Donate</a>
+        <a className="bg-v-black px-6 py-3 rounded-sm text-white">Join Us</a>
       </div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6 md:hidden"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M4 6h16M4 12h16m-7 6h7"
+        />
+      </svg>
     </nav>
   );
 };
