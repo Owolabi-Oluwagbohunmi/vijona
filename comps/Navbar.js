c9b78944c60580aaf4a99fd7f3826dcb33638840
unknown <samuelowolabi.89@gmail.com>
2021-10-04 08:41:03 +0100

    Navigation bar commit

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -3,7 +3,21 @@ import Link from 'next/link'
 
 
 const Navbar = () => {
+    <nav className='flex w-100 justify-between'>
+        <a>Vijona</a>
 
+        <div>
+            <a>Home</a>
+            <a>Home</a>
+            <a>Home</a>
+            <a>Home</a>
+        </div>
+
+        <div>
+            <a>Donate</a>
+            <a>Join Us</a>
+        </div>
+    </nav>
 }
  
 export default Navbar;
