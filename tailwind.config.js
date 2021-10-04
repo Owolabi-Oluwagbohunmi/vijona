a5d40e2aa5083d2fa82ff247216b3e1deb628db3
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-10-04 17:12:17 +0100

    Update tailwind configuration with colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,jsx}","./comp/**/*.{js,jsx}"],
+  purge: ["./pages/**/*.{js,jsx}","./comps/**/*.{js,jsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend:{
+      colors:{
+        "v-yellow":"#FBB040",
+        "v-green":"#0D4D4D",
+        "v-black":"#000000",
+        "bg-cta": '#D3E5E4',
+        "v-bg": '#F5F8FD'
+      } 
+    },
   },
   variants: {
     extend: {},
