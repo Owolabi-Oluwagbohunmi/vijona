0e56a43465ff7472512f97dd0199db9b3319fd9f
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-10-05 04:15:29 +0100

    Finishing adding navbar code

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -4,25 +4,25 @@ import Logo from '../public/assets/vijona-black.jpg';
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between py-8 items-center">
+    <nav className="flex justify-between py-8 items-center font-medium">
       <div className=" w-28 md:w-36 cursor-pointer border p-0 ">
-            <Image className="w-full  " src={Logo} alt="vijona-logo" />
+            <Image className="w-full" src={Logo} alt="vijona-logo" />
           </div>
 
-      <div className="hidden lg:flex">
+      <div className="hidden xl:flex font-medium">
         <a className="cursor-pointer mx-8 ">About Us</a>
         <a className="cursor-pointer mx-8 ">What we Do</a>
         <a className="cursor-pointer mx-8 ">Events & Stories</a>
         <a className="cursor-pointer mx-8">Alumni Community</a>
       </div>
 
-      <div className="hidden lg:flex">
-        <a className="bg-v-yellow mr-14 px-6 py-3 rounded-sm text-white">Donate</a>
-        <a className="bg-v-black px-6 py-3 rounded-sm text-white">Join Us</a>
+      <div className="hidden xl:flex">
+        <a className="bg-v-yellow mr-14 px-6 py-3 rounded-sm shadow-md text-white">Donate</a>
+        <a className="bg-v-black px-6 py-3 rounded-sm shadow-md  text-white">Join Us</a>
       </div>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6 md:hidden"
+        className="h-6 w-6 xl:hidden"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
