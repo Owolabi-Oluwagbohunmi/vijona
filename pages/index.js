e9ff1e4fea0cb6be7da0d33d2cd6b8f58d93dabb
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-10-05 04:17:33 +0100

    Add Hero section to index page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Hero from '../comps/Hero'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <Head>
       <title>Vijona | Home</title>
     </Head>
+    <Hero/>
 
     </>
   )
