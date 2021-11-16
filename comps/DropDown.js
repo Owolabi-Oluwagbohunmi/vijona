0e7fff53f685588d4503059cbb6c432a05f50ab6
unknown <samuelowolabi.89@gmail.com>
2021-11-16 19:59:59 +0100

    drp down

--- comps/DropDown.js
+++ comps/DropDown.js
@@ -1,11 +1,28 @@
 import React from "react";
 import Link from "next/link";
+import { MenuItemsDD } from "./DropDownMenuItems";
+import { useState } from "react";
 
-const DropDown = () => {
+const DropDown = () =>
+{
 
-  return (
-    <div>Hello</div>
-  );
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)
+
+    return (
+        <>
+            <ul onClick={handleClick} className={click ? "" : ""}>
+                {MenuItemsDD.map((MenuItem, index) => (
+                    <li key={index}>
+                        <Link href={MenuItem.path} onClick={setClick(false)}>
+                            {MenuItem.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 };
 
 export default DropDown;
---
+++ comps/DropDownMenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItemsDD = [
+    {
+        title : 'IGNITE Experience',
+        path : '/ignite-experience',
+        cName : 'dropdown-link'
+    },
+    {
+        title : 'IGNITE Experience',
+        path : '/ignite-experience',
+        cName : 'dropdown-link'
+    },
+    {
+        title : 'IGNITE Experience',
+        path : '/ignite-experience',
+        cName : 'dropdown-link'
+    }
+]
