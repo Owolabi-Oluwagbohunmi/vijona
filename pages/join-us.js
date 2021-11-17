f4231c3b56dae9cb0f263544694a8f2f79c76ef4
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-11-17 08:40:39 +0100

    Add Join us and join-us

---
+++ pages/join-us.js
@@ -0,0 +1,23 @@
+import Bullet from "../comps/Bullet";
+import Form from "../comps/Form";
+
+const joinUs = () => {
+  return (
+    <>
+      <section>
+        <div className="w-11/12 mx-auto xl:mt-48 mt-36 ">
+          <h1 className=" text-4xl md:text-6xl my-4 font-semibold ">
+            You can partner with us by :
+          </h1>
+          <div className="my-20">
+            <Bullet textVal="Volunteering" />
+            <Bullet textVal="By integrating one of our programs into your existing youth impact initiative" />
+          </div>
+        </div>
+        <Form/>
+      </section>
+    </>
+  );
+};
+
+export default joinUs;
