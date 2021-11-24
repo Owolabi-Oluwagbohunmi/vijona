57865560f12bbdbf31bda2a6085ab6bfa34d1c32
unknown <samuelowolabi.89@gmail.com>
2021-11-24 12:32:45 +0100

    events

--- pages/events.js
+++ pages/events.js
@@ -339,7 +339,6 @@ export default function events()
                   <span>Featured</span>
                 </div>
                 <h1 className="text-4xl font-bold leading-none lg:text-5xl xl:text-6xl"><a href="#_">Past Events and Ignite Experience Workshops</a></h1>
-                <p className="pt-2 text-sm font-medium">by <a href="#_" className="mr-1 underline">John Doe</a> · <span className="mx-1">April 23rd, 2021</span> · <span className="mx-1 text-gray-600">5 min. read</span></p>
               </div>
             </div>
           </div>
@@ -348,14 +347,13 @@ export default function events()
             <div className="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
               <a href="#_" className="block">
                 <Image className="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56" src={workshop1} />
-                {/* <img className="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56" src="https://cdn.devdojo.com/images/may2021/fruit.jpg"> */}
               </a>
               <div className="bg-purple-500 flex items-center px-3 py-1.5 leading-none rounded-full text-xs font-medium uppercase text-white inline-block">
-                <span>Lifestyle</span>
+                <span>Event</span>
               </div>
               <h2 className="text-lg font-bold sm:text-xl md:text-2xl"><a href="#_">Creating a Future Worth Living</a></h2>
-              <p className="text-sm text-gray-500">Learn the attributes you need to gain in order to build a future and create a life that you are truly happy with.</p>
-              <p className="pt-2 text-xs font-medium"><a href="#_" className="mr-1 underline">Mary Jane</a> · <span className="mx-1">April 17, 2021</span> · <span className="mx-1 text-gray-600">3 min. read</span></p>
+              <p className="text-sm text-gray-500">This gala held in california chicago to raise funds for cameroon indigenes.</p>
+              <p className="pt-2 text-xs font-medium"><a href="#_" className="mr-1 underline">California</a> · <span className="mx-1">April 17, 2021</span></p>
             </div>
 
             <div className="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
@@ -366,11 +364,11 @@ export default function events()
                 {/* <img className="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56" src="https://cdn.devdojo.com/images/may2021/workout.jpg"> */}
               </a>
               <div className="bg-pink-500 flex items-center px-3 py-1.5 leading-none rounded-full text-xs font-medium uppercase text-white inline-block">
-                <span>Health</span>
+              <span>Event</span>
               </div>
-              <h2 className="text-lg font-bold sm:text-xl md:text-2xl">The Healther Version of Yourself</h2>
-              <p className="text-sm text-gray-500">If you want to excel in life and become a better version of yourself, you'll need to upgrade your life.</p>
-              <p className="pt-2 text-xs font-medium"><a href="#_" className="mr-1 underline">Fred Jones</a> · <span className="mx-1">April 10, 2021</span> · <span className="mx-1 text-gray-600">3 min. read</span></p>
+              <h2 className="text-lg font-bold sm:text-xl md:text-2xl"><a href="#_">Creating a Future Worth Living</a></h2>
+              <p className="text-sm text-gray-500">This gala held in california chicago to raise funds for cameroon indigenes.</p>
+              <p className="pt-2 text-xs font-medium"><a href="#_" className="mr-1 underline">California</a> · <span className="mx-1">April 17, 2021</span></p>
             </div>
 
             <div className="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
@@ -381,11 +379,11 @@ export default function events()
                 {/* <img className="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56" src="https://cdn.devdojo.com/images/may2021/food.jpg"> */}
               </a>
               <div className="bg-red-500 flex items-center px-3 py-1.5 leading-none rounded-full text-xs font-medium uppercase text-white inline-block">
-                <span>Food</span>
+              <span>Event</span>
               </div>
-              <h2 className="text-lg font-bold sm:text-xl md:text-2xl">Enjoy the Meals of the Kings</h2>
-              <p className="text-sm text-gray-500">Take the time to enjoy the life that you've created. It's totally fine to live like kings and eat like royalty.</p>
-              <p className="pt-2 text-xs font-medium"><a href="#_" className="mr-1 underline">Mike Roberts</a> · <span className="mx-1">April 6, 2021</span> · <span className="mx-1 text-gray-600">3 min. read</span></p>
+              <h2 className="text-lg font-bold sm:text-xl md:text-2xl"><a href="#_">Creating a Future Worth Living</a></h2>
+              <p className="text-sm text-gray-500">This gala held in california chicago to raise funds for cameroon indigenes.</p>
+              <p className="pt-2 text-xs font-medium"><a href="#_" className="mr-1 underline">California</a> · <span className="mx-1">April 17, 2021</span></p>
             </div>
 
             <div className="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
@@ -393,14 +391,13 @@ export default function events()
                 <div className="object-cover w-full mb-2 overflow-hidden rounded-lg max-h-56" >
                   <Image src={workshop1} />
                 </div>
-                {/* <img className="object-cover w-full mb-2 overflow-hidden rounded-lg max-h-56" src="https://cdn.devdojo.com/images/may2021/books.jpg"> */}
               </a>
               <div className="bg-blue-500 flex items-center px-3 py-1.5 leading-none rounded-full text-xs font-medium uppercase text-white inline-block">
-                <span>Motivation</span>
+              <span>IGNITE workshop</span>
               </div>
-              <h2 className="text-lg font-bold sm:text-xl md:text-2xl">Writing for Success</h2>
-              <p className="text-sm text-gray-500">Writing about your plans for success is extremely helpful for yourself and it will allow you to share your story.</p>
-              <p className="pt-2 text-xs font-medium"><a href="#_" className="mr-1 underline">Tom Johnson</a> · <span className="mx-1">May 25, 2021</span> · <span className="mx-1 text-gray-600">3 min. read</span></p>
+              <h2 className="text-lg font-bold sm:text-xl md:text-2xl"><a href="#_">Creating a Future Worth Living</a></h2>
+              <p className="text-sm text-gray-500">This gala held in california chicago to raise funds for cameroon indigenes.</p>
+              <p className="pt-2 text-xs font-medium"><a href="#_" className="mr-1 underline">California</a> · <span className="mx-1">April 17, 2021</span></p>
             </div>
 
             <div className="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
@@ -412,11 +409,11 @@ export default function events()
                 {/* <img className="object-cover w-full mb-2 overflow-hidden rounded-lg max-h-56" src="https://cdn.devdojo.com/images/may2021/clock.jpg"> */}
               </a>
               <div className="bg-gray-800 flex items-center px-3 py-1.5 leading-none rounded-full text-xs font-medium uppercase text-white inline-block">
-                <span>Business</span>
+              <span>Event</span>
               </div>
-              <h2 className="text-lg font-bold sm:text-xl md:text-2xl">Simple Time Management</h2>
-              <p className="text-sm text-gray-500">Managing your time can make the difference between getting ahead in life or staying exactly where you are.</p>
-              <p className="pt-2 text-xs font-medium"><a href="#_" className="mr-1 underline">Scott Reedman</a> · <span className="mx-1">May 18, 2021</span> · <span className="mx-1 text-gray-600">3 min. read</span></p>
+              <h2 className="text-lg font-bold sm:text-xl md:text-2xl"><a href="#_">Creating a Future Worth Living</a></h2>
+              <p className="text-sm text-gray-500">This gala held in california chicago to raise funds for cameroon indigenes.</p>
+              <p className="pt-2 text-xs font-medium"><a href="#_" className="mr-1 underline">California</a> · <span className="mx-1">April 17, 2021</span></p>
             </div>
 
             <div className="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
@@ -424,14 +421,14 @@ export default function events()
                 <div className="object-cover w-full mb-2 overflow-hidden rounded-lg max-h-56" >
                   <Image src={workshop1}/>
                   </div>
-                {/* <img className="object-cover w-full mb-2 overflow-hidden rounded-lg max-h-56" src="https://cdn.devdojo.com/images/may2021/lemons.jpg"> */}
+    
               </a>
               <div className="bg-yellow-400 flex items-center px-3 py-1.5 leading-none rounded-full text-xs font-medium uppercase text-white inline-block">
-                <span>Nutrition</span>
+              <span>IGNITE workshop</span>
               </div>
-              <h2 className="text-lg font-bold sm:text-xl md:text-2xl">The Fruits Life</h2>
-              <p className="text-sm text-gray-500">Take a moment and enjoy to enjoy the many fruits of life. Relaxation and a healthy diet can go a long way.</p>
-              <p className="pt-2 text-xs font-medium"><a href="#_" className="mr-1 underline">Jake Caldwell</a> · <span className="mx-1">May 15, 2021</span> · <span className="mx-1 text-gray-600">3 min. read</span></p>
+              <h2 className="text-lg font-bold sm:text-xl md:text-2xl"><a href="#_">Creating a Future Worth Living</a></h2>
+              <p className="text-sm text-gray-500">This gala held in california chicago to raise funds for cameroon indigenes.</p>
+              <p className="pt-2 text-xs font-medium"><a href="#_" className="mr-1 underline">California</a> · <span className="mx-1">April 17, 2021</span></p>
             </div>
 
           </div>
