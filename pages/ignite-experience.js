277795c1fc061ad6907db131b45842b52e8e54f6
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-11-25 08:32:30 +0100

    Fix Our solution responsiveness

--- pages/ignite-experience.js
+++ pages/ignite-experience.js
@@ -59,19 +59,19 @@ const igniteExperience = () =>
         <div className="px-20 mx-auto max-w-7xl">
           <h2 className="mb-1 text-3xl font-extrabold leading-tight text-gray-900">Our Solution</h2>
           <p className="mb-16 text-lg text-gray-500">Imagine if young africans are equipped early on in life.</p>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-16 lg:gap-x-24 gap-y-20">
+          <div className="grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 gap-10 lg:gap-20">
             <div>
               <div className="flex items-center justify-center w-12 h-12 mb-4 text-red-600 bg-red-100 rounded-full">
                 <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"></path>
                 </svg>
               </div>
-              <h3 className="mb-2 text-base font-semibold leading-tight text-gray-900 lg:text-lg">Gifted with self discovery <br />Who am I</h3>
+              <h3 className="mb-2 text-xl font-semibold leading-tight text-gray-900 lg:text-lg">Gifted with self discovery <span className="block">Who am I</span></h3>
               <div className="text-sm text-gray-500 lg:text-base">
 
                 <p>1. Innate VALUE; talents, and abilities from birth.Think criically and creatively </p>
-                <br />
-                <p>2. Lifelong paradigm shift</p>
+    
+                <p className="block my-3">2. Lifelong paradigm shift</p>
 
               </div>
             </div>
@@ -83,12 +83,12 @@ const igniteExperience = () =>
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
                 </svg>
               </div>
-              <h3 className="mb-2 text-base font-semibold leading-tight text-gray-900 lg:text-lg">Exposed to contexual Occupations What can i do</h3>
+              <h3 className="mb-2 text-xl font-semibold leading-tight text-gray-900 lg:text-lg">Exposed to contexual Occupations What can i do</h3>
               <div className="text-sm text-gray-500 lg:text-base">
 
                 <p>3. Relevant occupations that align with who they are</p>
-                <br />
-                <p>4. Operate from a place of passion; the birthplace of innovation</p>
+                
+                <p className="block my-3">4. Operate from a place of passion; the birthplace of innovation</p>
 
               </div>
             </div>
@@ -99,12 +99,12 @@ const igniteExperience = () =>
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path>
                 </svg>
               </div>
-              <h3 className="mb-2 text-base font-semibold leading-tight text-gray-900 lg:text-lg">Enabled to draft life plans</h3>
+              <h3 className="mb-2 text-xl font-semibold leading-tight text-gray-900 lg:text-lg">Enabled to draft life plans</h3>
               <div className="text-sm text-gray-500 lg:text-base">
 
                 <p>5. Actionable and SMART goals</p>
-                <br />
-                <p>6. Becoming self-advocates of their success</p>
+                
+                <p className="block my-3">6. Becoming self-advocates of their success</p>
 
               </div>
             </div>
@@ -115,11 +115,11 @@ const igniteExperience = () =>
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"></path>
                 </svg>
               </div>
-              <h3 className="mb-2 text-base font-semibold leading-tight text-gray-900 lg:text-lg">Empowered to achieve their plans</h3>
+              <h3 className="mb-2 text-xl font-semibold leading-tight text-gray-900 lg:text-lg">Empowered to achieve their plans</h3>
               <div className="text-sm text-gray-500 lg:text-base">
 
                 <p>7. Effective programs, relevant trainings, mentorahip network, and exposure</p>
-                <br />
+               
 
               </div>
             </div>
@@ -134,13 +134,13 @@ const igniteExperience = () =>
               <p className="text-sm text-gray-500 lg:text-base">Simple and configuration intregrations with your favorite applications and services.</p>
             </div> */}
 
-            <div className="col-span-2">
+            <div className="">
               <div className="flex items-center justify-center w-12 h-12 mb-4 text-pink-600 bg-pink-100 rounded-full">
                 <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path>
                 </svg>
               </div>
-              <h3 className="mb-2 text-base font-semibold leading-tight text-gray-900 lg:text-lg">SKiiled Africans for Africa.</h3>
+              <h3 className="mb-2 text-xl font-semibold leading-tight text-gray-900 lg:text-lg">SKiiled Africans for Africa.</h3>
               <p className="text-sm text-gray-500 lg:text-base">Outcome</p>
             </div>
 
