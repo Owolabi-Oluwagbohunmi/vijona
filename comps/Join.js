16752d901e1b65b34bac2cc607f43fc5e8a9a987
unknown <samuelowolabi.89@gmail.com>
2021-11-26 15:12:47 +0100

    done again

--- comps/Join.js
+++ comps/Join.js
@@ -9,28 +9,18 @@ import donateIcon from "../public/assets/heart.png";
 import volunteerIcon from "../public/assets/help.png";
 import buildUrl from "cloudinary-build-url";
 
-const Join = () => {
+const Join = () =>
+{
   const [showForm, setShowForm] = useState(false);
 
-  const partners = buildUrl("vijonapple_wfoptm",{
-    cloud:{
+  const partners = buildUrl("vijonapple_wfoptm", {
+    cloud: {
       cloudName: "dbwk2jksa"
     }
   })
   return (
     <>
       <section>
-        {/* <div className="w-11/12 mx-auto xl:pt-48 pt-36 ">
-          <h1 className=" text-4xl md:text-6xl my-4 font-semibold ">
-            You can partner with us by :
-          </h1>
-          <div className="my-20">
-            <Bullet textVal="Volunteering" />
-            <Bullet textVal="By integrating one of our programs into your existing youth impact initiative" />
-          </div>
-        </div>
-        <Form /> */}
-
         <div className="relative text-center">
           <div className="mix-blend-overlay pt-24">
             <Image
@@ -41,7 +31,7 @@ const Join = () => {
             ></Image>
           </div>
 
-          <div className=" bg-black py-32  opacity-60 mx-auto w-full text-white">
+          <div className=" bg-black py-64  opacity-60 mx-auto w-full text-white">
             <h1 className=" text-6xl font-semibold text-white capitalize my-5">
               Join us in actualizing this great feat
             </h1>
